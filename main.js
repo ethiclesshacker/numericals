5410a83af8f0e235bef438efd39580466e1aaa58
ethiclesshacker <mailingadityavs@gmail.com>
2021-02-06 11:58:25 +0530

    Adding JS functions and taking equations.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("Hey Connection!");
+
+
+function bisection(eqn, a, b, dp) {
+    mid = (a + b) / 2;
+    x = mid;
+    value = eval(eqn);
+    value = parseFloat(value.toFixed(2));
+    console.log(`a=${a}, b=${b}, mid=${mid}`);
+    console.log(value);
+}
+
+bisection("(x**3)-(3*x)+1.06", 0, 1);
